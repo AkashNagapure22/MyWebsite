551865a703e4a0589d4c2385caf3e152ee42c376
AkashNagapure22 <107910612+AkashNagapure22@users.noreply.github.com>
2026-08-25 13:52:49 +0000

    A

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.akashnagapure.in',
+  build: {
+    format: 'file'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
